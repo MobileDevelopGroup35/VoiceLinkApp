f6867f48c402e60e0008e162587548f81f5dffa1
t3mina00 <nmine23@student.oulu.fi>
2025-04-03 19:24:22 +0300

    Add codes for theming

--- app/src/main/java/com/l4kt/voicelink/ui/theme/Color.kt
+++ app/src/main/java/com/l4kt/voicelink/ui/theme/Color.kt
@@ -2,10 +2,63 @@ package com.l4kt.voicelink.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Primary colors
+val PrimaryLight = Color(0xFF4F46E5)
+val OnPrimaryLight = Color(0xFFFFFFFF)
+val PrimaryContainerLight = Color(0xFFE0E0FF)
+val OnPrimaryContainerLight = Color(0xFF000F5C)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Secondary colors
+val SecondaryLight = Color(0xFF6B49C3)
+val OnSecondaryLight = Color(0xFFFFFFFF)
+val SecondaryContainerLight = Color(0xFFEADDFF)
+val OnSecondaryContainerLight = Color(0xFF25005A)
+
+// Tertiary colors
+val TertiaryLight = Color(0xFF7D5700)
+val OnTertiaryLight = Color(0xFFFFFFFF)
+val TertiaryContainerLight = Color(0xFFFFDEA8)
+val OnTertiaryContainerLight = Color(0xFF271900)
+
+// Error colors
+val ErrorLight = Color(0xFFBA1A1A)
+val OnErrorLight = Color(0xFFFFFFFF)
+val ErrorContainerLight = Color(0xFFFFDAD6)
+val OnErrorContainerLight = Color(0xFF410002)
+
+// Background colors
+val BackgroundLight = Color(0xFFFFFBFF)
+val OnBackgroundLight = Color(0xFF1C1B1E)
+val SurfaceLight = Color(0xFFFFFBFF)
+val OnSurfaceLight = Color(0xFF1C1B1E)
+
+// Dark theme colors
+val PrimaryDark = Color(0xFFBEC2FF)
+val OnPrimaryDark = Color(0xFF000F5C)
+val PrimaryContainerDark = Color(0xFF293193)
+val OnPrimaryContainerDark = Color(0xFFE0E0FF)
+
+val SecondaryDark = Color(0xFFD0BCFF)
+val OnSecondaryDark = Color(0xFF381E72)
+val SecondaryContainerDark = Color(0xFF4F378B)
+val OnSecondaryContainerDark = Color(0xFFEADDFF)
+
+val TertiaryDark = Color(0xFFF0C148)
+val OnTertiaryDark = Color(0xFF422D00)
+val TertiaryContainerDark = Color(0xFF5F4200)
+val OnTertiaryContainerDark = Color(0xFFFFDEA8)
+
+val ErrorDark = Color(0xFFFFB4AB)
+val OnErrorDark = Color(0xFF690005)
+val ErrorContainerDark = Color(0xFF93000A)
+val OnErrorContainerDark = Color(0xFFFFDAD6)
+
+val BackgroundDark = Color(0xFF1C1B1E)
+val OnBackgroundDark = Color(0xFFE6E1E6)
+val SurfaceDark = Color(0xFF1C1B1E)
+val OnSurfaceDark = Color(0xFFE6E1E6)
+
+// Priority colors
+//val LowPriority = Color(0xFF4CAF50)
+//val MediumPriority = Color(0xFFFFC107)
+//val HighPriority = Color(0xFFE53935)
